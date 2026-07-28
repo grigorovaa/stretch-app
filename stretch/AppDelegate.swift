a18719719ed62dec72168f08160fb69c0379d581
Marina Grigorova <grigorovaa.marina@gmail.com>
2026-07-28 23:15:25 +0300

    Initial Commit

---
+++ stretch/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  stretch
+//
+//  Created by Marina Grigorova on 28.7.2026.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ stretch/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  stretch
+//
+//  Created by Marina Grigorova on 28.7.2026.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
